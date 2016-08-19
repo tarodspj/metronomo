9a5fc2b998894293616f33309fc771516c8a9612
Regueiro, Roberto <tarod_spj@hotmail.com>
2016-08-19 18:25:18 +0200

    reordenar codigo

--- js/modules/start.js
+++ js/modules/start.js
@@ -3,24 +3,64 @@ define(['jquery', 'domready', 'modules/drawTimes','modules/checkAudio', 'modules
     var timer, audioCtx, noteCount, cuantosDots;
     var delta = 0;
     var curTime = 0.0;
+
     var AudioContext =  checkAudio;
+
+    function schedule() {
+
+      while(curTime < audioCtx.currentTime + 0.1) {
+        playNote.play(curTime);
+        updateTime();
+      }
+
+      timer = window.setTimeout(schedule, 0.1);
+    }
+
+    function updateTime() {
+      curTime +=  1;
+      noteCount++;
+    }
 
     function listeners() {
       $('.tempoData').on('change', function() {
 
-          var idChanged =   $( this ).attr( 'class' );
-          idChanged =       idChanged.replace( ' tempoData', '' );
+          var idChanged = $(this).attr('class');
+          idChanged =  idChanged.replace(' tempoData', '');
 
           //drawTimes(idChanged);
           drawTimes.draw(idChanged);
       });
+
       $('.controls').on('click', function() {
           $('body' ).addClass('overlay');
           $('.play-btn' ).removeClass('play');
           window.clearInterval(timer);
       });
+
       $('#closeOverlay').on('click', function() {
           $('body').removeClass('overlay');
+      });
+
+      $('.play-btn').on('click', function() {
+
+        if($(this).data('what') === 'pause') {
+          // ====== Pause ====== //
+          window.clearInterval(timer);
+
+          $('.counter .dot').attr('style', '');
+          $( this ).data('what', 'play').removeClass('play');//.text( 'Play' );
+
+        }
+        else {
+          // ====== Play ====== //
+          curTime =       audioCtx.currentTime;
+          noteCount =     parseInt( $('.dot').length);
+          cuantosDots =   noteCount;
+          schedule();
+
+          $(this).data('what', 'pause').addClass('play');//.text( 'Stop' );
+
+        }
       });
     }
 
@@ -44,49 +84,6 @@ define(['jquery', 'domready', 'modules/drawTimes','modules/checkAudio', 'modules
     else {
         $('body').append('<h1>Sorry, but the Web Audio API is not supported by your browser. Please, consider upgrading to the latest version or downloading Google Chrome or Mozilla Firefox</h1>');
     }
-
-    function schedule() {
-
-      while(curTime < audioCtx.currentTime + 0.1) {
-        playNote.play(curTime);
-        updateTime();
-      }
-
-      timer = window.setTimeout(schedule, 0.1);
-    }
-
-    function updateTime() {
-      curTime +=  1;
-      noteCount++;
-    }
-
-    /* Play note on a delayed interval of t */
-
-
-    /* Play and stop button */
-    $( '.play-btn' ).on('click', function() {
-
-      if( $( this ).data( 'what' ) === 'pause' ) {
-        // ====== Pause ====== //
-        window.clearInterval( timer );
-
-        $( '.counter .dot' ).attr( 'style', '' );
-        $( this ).data( 'what', 'play' ).removeClass( 'play' );//.text( 'Play' );
-
-      }
-      else {
-        // ====== Play ====== //
-        curTime =       audioCtx.currentTime;
-        noteCount =     parseInt( $( '.dot' ).length );
-        cuantosDots =   parseInt( $( '.dot' ).length );
-        schedule();
-
-        $( this ).data( 'what', 'pause' ).addClass( 'play' );//.text( 'Stop' );
-
-      }
-    });
-
-    // Load up dots on pageload
 
 
 });
